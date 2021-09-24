a35ddb790446518f20c5b3383c17f00ec703ff37
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 22:39:36 +0530

    All users function done

--- server/users.js
+++ server/users.js
@@ -19,6 +19,10 @@ const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id);
 
     if (index !== -1) {
-        users.splice(index, 1)[0];
+       return users.splice(index, 1)[0];
     }
 }
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUserInRoom = (room) => users.filter((user) => user.room === room);
