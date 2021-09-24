8cf30076ce3071a11fac90286ab78fd8e3f814ca
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 22:35:04 +0530

    Create remove user function

--- server/users.js
+++ server/users.js
@@ -14,3 +14,11 @@ const addUser = ({ id, name, room}) => {
     users.push(user);
     return { user };
 }
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index !== -1) {
+        users.splice(index, 1)[0];
+    }
+}
