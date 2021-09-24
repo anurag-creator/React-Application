983b4a7ac414bf9eaadee11f651b61b4d06dc44b
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 17:58:26 +0530

    Socket connection done front-end to backend

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
-    return (
-        <h1>Chat</h1>
-    )
-}
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'http://localhost:5000/';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    setName(name);
+    setRoom(room);
+
+    let socket = io(ENDPOINT, { transports: ['websocket'] });
+      console.log(socket);
+  });
+  return <h1>Chat</h1>;
+};
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
@@ -17,7 +18,7 @@ io.on('connection', (socket) => {
         console.log('User had left');
     })
 })
-
+app.use(cors());
 app.use(router);
 server.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
