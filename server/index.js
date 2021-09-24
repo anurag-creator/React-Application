e225da8a242659ffcfa6e3809b3d46217d2caf06
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 16:22:33 +0530

    Backend server started

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+server.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+})
