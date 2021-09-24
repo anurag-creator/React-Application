e4690150a9fbdbc5685d85134090273a7f47088e
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 16:59:05 +0530

    server started and make socket connection

--- server/index.js
+++ server/index.js
@@ -3,12 +3,22 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 5000;
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('We have a new Connection', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('User had left');
+    })
+})
+
+app.use(router);
 server.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
 })
