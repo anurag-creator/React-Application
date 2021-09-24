d45f60f40b68f84939ba9d58d77679929be18e52
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 16:07:49 +0530

    Create App component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => (
+    <Router>
+        <Route path="/" exact component = {Join}/>
+        <Route path="/Chat"  component = {Chat}/>
+    </Router>
+)
