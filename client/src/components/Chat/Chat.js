f7db79dbd43bf03d37f68a7241b6454e8cfc6ef5
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-14 20:05:05 +0530

    Create Admin message function

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,26 +5,33 @@ import io from 'socket.io-client';
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'http://localhost:5000/';
+  let socket = io(ENDPOINT, { transports: ['websocket'] });
+
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    let socket = io(ENDPOINT, { transports: ['websocket'] });
     //   console.log(socket);
     setName(name);
     setRoom(room);
-    socket.emit('join', { name, room }, () => {
-      
-    });
+    socket.emit('join', { name, room }, () => {});
 
     return () => {
       socket.emit('disconnect');
 
       socket.off();
-    }
-
+    };
   }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
   return <h1>Chat</h1>;
 };
 
--- server/index.js
+++ server/index.js
@@ -21,8 +21,21 @@ io.on('connection', (socket) => {
     if (error) {
       return callback(error);
     }
+
+    socket.emit('message', { user: 'Admin', text: `${user.name}, Welcome to the room ${user.room}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'Admin', text: `${user.name}, has joined! ` });
+
     socket.join(user.room);
+    callback();
   });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    callback();
+  })
+
 
   socket.on('disconnect', () => {
     console.log('User had left');
