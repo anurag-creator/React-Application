caa31215f41d8c71d44c7c23d99293f4bccf82a7
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-21 11:28:12 +0530

    Chat Component done

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,58 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+
+let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'http://localhost:5000/';
-  let socket = io(ENDPOINT, { transports: ['websocket'] });
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    //   console.log(socket);
-    setName(name);
+    socket = io(ENDPOINT);
+
     setRoom(room);
-    socket.emit('join', { name, room }, () => {});
+    setName(name)
 
-    return () => {
-      socket.emit('disconnect');
-
-      socket.off();
-    };
-  }, [ENDPOINT, location.search]);
-
-  useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message]);
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
-  }, [messages]);
-
-  // Function to send Message
+  }, [ENDPOINT, location.search]);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
-    if (message) {
+
+    if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
-  console.log(message, messages);
+
   return (
     <div className="outerContainer">
       <div className="container">
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) =>
-            event.key === 'Enter' ? sendMessage(event) : null
-          }
-        />
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users}/>
     </div>
   );
-};
+}
 
 export default Chat;
