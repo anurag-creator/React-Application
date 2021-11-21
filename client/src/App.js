f31e9ba30bb574057d231e2615df9357eea78d87
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-21 11:24:33 +0530

    Changes in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
-const App = () => (
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
     <Router>
-        <Route path="/" exact component = {Join}/>
-        <Route path="/Chat"  component = {Chat}/>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
-)
+  );
+}
 
 export default App;
